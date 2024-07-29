e1c20eb58e4076f586a88f52135c1c0d1b20df38
zetdou <zyziupoz@gmail.com>
2024-07-29 21:57:47 +0200

    main.jsx
    Set correct basename to work with gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/goit-react-hw-08-phonebook/",
   build: {
     outDir: "./dist",
   },
